d3bdee040e013aed3a0de0142f87df8679f12967
KristelSalas <61443460+KristelSalas@users.noreply.github.com>
2023-10-09 20:00:29 -0600

    nadaaa

--- src/index.js
+++ src/index.js
@@ -1,10 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import ForgotPassword from './pages/forgotPassword/ForgotPassword';
+import Manuals from './pages/manuals/Manuals';
 import NewCount from './pages/newCount/NewCount';
+import RegisteredMicroorganisms from './pages/registeredMicroorganisms/RegisteredMicroorganisms';
+import RegisteredUsers from './pages/registeredUsers/RegisteredUsers';
+import { Login } from '@mui/icons-material';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <NewCount />
+    { < ForgotPassword /> }  
+    {/* < Manuals /> */}  
+    {/* < NewCount /> */}  
+    {/* < RegisteredMicroorganisms /> */}  
+    { /*< RegisteredUsers /> */}  
   </React.StrictMode>
 );
